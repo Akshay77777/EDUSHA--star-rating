a295f582ca4a62d7fd806de7502ea131f9c61d92
Akshay77777 <36538675+Akshay77777@users.noreply.github.com>
2021-08-21 12:07:23 +0530

    Add files via upload

--- JS/rating.js
+++ JS/rating.js
@@ -1,1 +1,36 @@
+const stars = document.querySelectorAll('.star');
+const output = document.querySelector('.output');
 
+for(x=0; x<stars.length; x++){
+    stars[x].starValue = (x+1);
+
+    ["click", "mouseover", "mouseout"].forEach(function(e){
+        stars[x].addEventListener(e, showRating);
+    })
+}
+
+function showRating(e){
+    let type = e.type;
+    let starValue = this.starValue;
+
+    stars.forEach(function(elem, ind){
+        if(type === 'click'){
+            if(ind < starValue){
+                elem.classList.add("orange");
+            }else{
+                elem.classList.remove("orange");
+            }
+        }
+        if(type === 'mouseover'){
+            if(ind < starValue){
+                elem.classList.add("yellow");
+            }else{
+                elem.classList.remove("yellow");
+            }
+        }
+        if(type === 'mouseout'){
+            
+                elem.classList.remove("yellow");
+        }
+    })
+}
